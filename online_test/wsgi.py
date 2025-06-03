1bc7b2353e6693f8716e79bee3534efa44ce340b
OS-03 <ferozshaikh2222@gmail.com>
2025-06-04 04:35:59 +0530

    changes

--- online_test/wsgi.py
+++ online_test/wsgi.py
@@ -1,11 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-from pathlib import Path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_test.settings')
-
 application = get_wsgi_application()
-static_root = Path(__file__).resolve().parent.parent / 'staticfiles'
-application = WhiteNoise(application, root=str(static_root))
 
